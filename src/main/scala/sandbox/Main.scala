4d57ab39e2c80a7807492a7f852507cf673f257a
Juan José <juanjosemoralesarias@gmail.com>
2018-02-05 22:22:05 -0500

    Restore project structure

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,6 +1,7 @@
 package sandbox
 
 import cats.Show
+import cats.Eq
 import cats.syntax.semigroup._
 import sandbox.domain.Cat
 import sandbox.lib.PrintableSyntax._
@@ -10,6 +11,7 @@ import cats.instances.int._
 import cats.instances.string._
 import cats.instances.option._
 import cats.syntax.show._
+import cats.syntax.eq._
 
 
 object Main extends App {
@@ -39,5 +41,31 @@ object Main extends App {
 
   println(lola.show)
 
-}
+  //Eq examples
+  val eqInt = Eq[Int]
 
+  println(eqInt.eqv(123,321)) //Comparing Ints false
+  println(eqInt.eqv(123,123)) //Compare Ints true
+
+ //Eq using syntax
+  println(123 === 123)
+  println(323 === 444)
+
+ //Comparing options
+  println((Some(1):Option[Int]) === (None:Option[Int])) //false
+  println((Some(1):Option[Int]) === (Some(1):Option[Int])) //true
+
+  //Comparing custom types
+
+  implicit val catEq:Eq[Cat] = Eq.instance[Cat]{
+    (cat1,cat2) => cat1.age === cat2.age && cat1.color === cat2.color && cat1.name === cat2.name
+
+  }
+
+  val lola2 = Cat("Lola",5,"White")
+
+  println(lola === lola2)
+  println(lola === Cat("Lola",5,"Black"))
+
+
+}
