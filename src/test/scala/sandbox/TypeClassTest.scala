bc4f7c14712e6b56d1c10c176b21626133dfc17b
Juan Jose Morales <juanjosemorales@MacBook-Pro-de-Juan.local>
2018-02-19 16:53:21 -0500

    Basic test for type class

--- src/test/scala/sandbox/TypeClassTest.scala
+++ src/test/scala/sandbox/TypeClassTest.scala
@@ -1,8 +1,28 @@
 package sandbox
 
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, Matchers}
+import sandbox.domain.Cat
+import sandbox.lib.Printable
 
-class TypeClassTest extends FlatSpec{
+class TypeClassTest extends FlatSpec with Matchers{
+
+  val lola = Cat("Lola",5,"White")
+
+
+  "A printable type class " should " format using printable object" in{
+    import sandbox.lib.PrintableInstances._
+
+    Printable.format(lola) shouldBe s"${lola.name} is ${lola.age} year old and  ${lola.color} cat.!"
+
+  }
+
+  "A printable type class " should " format using syntax for Cat type" in{
+    import sandbox.lib.PrintableInstances._
+    import sandbox.lib.PrintableSyntax._
+
+    Cat("Lola",5,"White").format shouldBe s"${lola.name} is ${lola.age} year old and  ${lola.color} cat.!"
+
+  }
 
 
 }
