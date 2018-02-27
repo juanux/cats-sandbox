abb69fbab619e060fe4e440703b7f7c392401798
Juan Jose Morales <juanjosemorales@gmail.com>
2018-02-26 21:23:53 -0500

    Add scalafmt and variance test

--- src/test/scala/sandbox/Variance.scala
+++ src/test/scala/sandbox/Variance.scala
@@ -4,12 +4,41 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Variance extends FlatSpec with Matchers{
 
-  trait Animals
-  case class Mamals() extends Animals
-  case class Fish() extends Animals
-  case class Cow() extends Mamals
-  case class Dolphin() extends Fish
+  trait Animal{
+    def getName():String
+  }
+  trait Mamals extends Animal
+  trait Fish extends Animal
+  case class Cow() extends Mamals{
+    override  def getName():String = "Cow"
+  }
+  case class Dolphin() extends Fish{
+    override  def getName():String = "Dolphin"
 
+  }
+
+  def getAnimalName(animal:Animal): String ={
+
+    animal.getName
+
+  }
+
+
+  "getAnimalName" should " return Fish string " in {
+
+    val fish = Dolphin()
+
+    getAnimalName(fish) shouldBe "Dolphin"
+
+  }
+
+  "getAnimalName" should " return Cow string " in {
+
+    val cow = Cow()
+
+    getAnimalName(cow) shouldBe "Cow"
+
+  }
 
 
 	
