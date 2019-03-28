7fdf7f88151ce76e8cb75e8bbb9dfd5171e3205c
Juan Jose Morales <juanjosemoralesarias@gmail.com>
2019-03-28 20:07:41 +0100

    rename option

--- src/test/scala/sandbox/Contravariance.scala
+++ src/test/scala/sandbox/Contravariance.scala
@@ -7,31 +7,17 @@ class Contravariance extends FlatSpec with Matchers{
   class Person {}
   class User extends Person{}
   class Admin extends User{}
-
-
   class SystemUser[-T]
-
-
 
   def setUser(box:SystemUser[Admin]):Option[String] = {
     None
   }
-
-
 
   it should " create a UserSystem receiving a User for a parametrized Admin" in {
 
     setUser(new SystemUser[Admin]) shouldBe None
     setUser(new SystemUser[User])   shouldBe None
 
-
   }
-
-
-
-
-
-
-
 
 }
