16b7dd8f6e44d3246d87795cd1b983f2b2a3119c
Juan Jose Morales <juanjosemoralesarias@gmail.com>
2019-10-19 14:15:08 +0200

    update excercises

--- src/test/scala/sandbox/algorithms/SubstringSearch.scala
+++ src/test/scala/sandbox/algorithms/SubstringSearch.scala
@@ -4,6 +4,15 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class SubstringSearch extends FlatSpec with Matchers {
 
+  it should "find a pattern in a specific text " in {
+
+    val text = "Hola como estas, yo muy bien y tu que tal"
+    val pattern = "yo muy bien"
+
+    SubstringSearch.naiveSubstringSearch("Hola como estas, yo muy bien y tu que tal", "yo muy bien")
+    text.indexOf(pattern)
+  }
+
 }
 
 object SubstringSearch {
---
+++ src/test/scala/sandbox/algorithms/test.sc
@@ -0,0 +1,9 @@
+val listSequency:List[Int] = List(1,6,10,4,7,9,5)
+
+
+listSequency.foldLeft(0)(_ - _)
+
+listSequency.foldRight(0)(_ - _)
+
+
+
